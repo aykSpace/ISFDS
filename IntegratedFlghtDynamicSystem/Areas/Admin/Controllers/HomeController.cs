4e05604b03f426de966f81c6bb4a3b6c04e5d07b
aykSpace <a.y.kutomanov@gmail.com>
2014-03-16 15:44:06 +0400

    Admin controller was added

---
+++ IntegratedFlghtDynamicSystem/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using IntegratedFlghtDynamicSystem.Areas.Default.ViewModels;
+using IntegratedFlghtDynamicSystem.Controllers;
+using IntegratedFlghtDynamicSystem.Models;
+
+namespace IntegratedFlghtDynamicSystem.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        //
+        // GET: /Admin/Home/
+
+        public ActionResult Index()
+        {
+            var spacecrafts = UnitOfWork.SpacecraftInfoRepository.Get();
+
+            var spcrVm =
+                spacecrafts.Select(
+                    spcr => (SpacecraftViewModel)Mapper.Map(spcr, typeof(SpacecraftInitialData), typeof(SpacecraftViewModel))).ToList();
+            return View(spcrVm);
+        }
+
+        public ActionResult AddSpacecraft()
+        {
+            return View();
+        }
+
+    }
+}
